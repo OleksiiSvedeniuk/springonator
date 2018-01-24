7317a01817276bf2f4154eed81067807aa718e33
OleksiiSvedeniuk <ljosha06@gmail.com>
2018-01-24 13:10:07 +0200

    Create springboot project

---
+++ src/main/java/springboot/springonator/SpringonatorApplication.java
@@ -0,0 +1,12 @@
+package springboot.springonator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringonatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringonatorApplication.class, args);
+	}
+}
---
+++ src/test/java/springboot/springonator/SpringonatorApplicationTests.java
@@ -0,0 +1,16 @@
+package springboot.springonator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringonatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
